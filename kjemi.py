f546cdbdcf0fc6d1ee2e9ecc184f467333022655
vemkro <vemund763@gmail.com>
2024-02-20 10:46:06 +0100

    added functions

--- kjemi.py
+++ kjemi.py
@@ -1,3 +1,12 @@
 import numpy as np
 
 # kjemiformler
+
+
+def konsentrasjon(stoffmengde, volum):
+    return stoffmengde / volum
+
+
+def stoffmengde(masse, molarMasse):
+    return masse / molarMasse
+
