d08a67cb19829b56cdff7d0f820380c03392a725
vemkro <vemund763@gmail.com>
2024-02-20 13:58:57 +0100

    la til kommentarer på kode

--- kjemi.py
+++ kjemi.py
@@ -5,23 +5,29 @@ import numpy as np
 
 
 def konsentrasjon(stoffmengde, volum):
+    """finne konsentrasjon hvis vi vet stoffmengde og volum"""
     return stoffmengde / volum
 
 
 def stoffmengde(masse, molarMasse):
+    """finne storrmengde ved å bruke massen og den molare massen"""
     return masse / molarMasse
 
 
 def stoffmengde_konsentrasjon(konsentrasjon, volum):
+    """finne stoffmengde ved at vi vet konsentrasjon og volum"""
     return konsentrasjon * volum
 
 
 def pH(konsentrasjon):
+    """beregne pH verdi ut i fra konsentrasjon av H3O+"""
     return -np.log10(konsentrasjon)
 
 
 def buffer_pH(pKa, nBase, nSyre):
+    """finne pH i en buffer"""
     return pKa + (np.log10((nBase / nSyre)))
+
 
 def test(tall1, tall2):
     return tall1 + tall2
